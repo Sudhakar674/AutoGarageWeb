a259c93616bbf029da7fe93a2c7ec7fe78909fa1
Sudhakar674 <sudhakar@massnexgen.com>
2024-10-18 17:39:03 +0530

    changes

---
+++ AutoGarageWeb/Models/Connection.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+
+namespace AutoGarageWeb.Models
+{
+    public class Connection
+    {
+        public static string connectionString = ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString;                                            
+ 
+        public static DataSet ExecuteQuery(string commandText, params SqlParameter[] parameters)
+        {
+            DataSet ds = new DataSet();
+            try
+            {
+                using (var connection = new SqlConnection(connectionString))
+                using (var command = new SqlCommand(commandText, connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddRange(parameters);
+                    SqlDataAdapter da = new SqlDataAdapter(command);
+                    da.Fill(ds);
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                DataTable dt = new DataTable();
+                dt.Columns.Add("Msg");
+                dt.Columns.Add("ErrorMessage");
+
+                DataRow dr = dt.NewRow();
+                dr["Msg"] = "0";
+                dr["ErrorMessage"] = ex.Message;
+                dt.Rows.Add(dr);
+                ds.Tables.Add(dt);
+            }
+            return ds;
+        }
+    }
+}
